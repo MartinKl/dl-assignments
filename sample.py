74f32cb080a7806980a1fe8f01b637a46206c167
Martin Klotz <MartinKl@hu-berlin.de>
2016-05-06 11:29:40 +0200

    started sampling, well, not really

--- sample.py
+++ sample.py
@@ -1,7 +1,24 @@
 from blocks.serialization import load
 from blocks.filter import VariableFilter
 
+from theano import function
+
 f = open('model.bin', 'rb')
 main_loop = load(f)
 model = main_loop.model
-print(dir(main_loop.model()))
+outname = 'softmax_log_probabilities_output'
+xname = 'x'
+soft_outs = None
+x = None
+i = 0
+while (not soft_outs or not x) and i<len(model.variables):
+    var_i = model.variables[i]
+    soft_outs = var_i if var_i.name == outname else None
+    x = var_i if var_i.name == xname else None
+    if var_i.name == outname:
+        soft_outs = var_i
+    elif var_i.name == xname:
+        x = var_i
+    i += 1
+
+f = function(x, soft_outs)
